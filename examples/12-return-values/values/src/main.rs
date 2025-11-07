b40ee0e0ed449738b0aed96c6c73015d62c21778
alessio marganiit.it <alessio.margan@iit.it>
2025-11-07 23:26:32 +0100

    Adds Rust development environment
    Sets up a Rust development environment using a devcontainer.
    
    Introduces a basic Rust project structure for testing purposes.
    
    Updates an example to handle potential `None` values and adds a function to calculate the mean of a number slice.

--- examples/12-return-values/values/src/main.rs
+++ examples/12-return-values/values/src/main.rs
@@ -3,10 +3,12 @@ fn split_string(s: String, delimiter: char, field: usize) -> String {
     let parts: Vec<&str> = s.split(delimiter).collect();
     // This will not compile!
     let result = parts.get(field);
-    result.to_string()
+    result.expect("handle None option").to_string()
 }
 
 fn main() {
-    let chunk = split_string("hello,world".to_string(), ',', 1);
+    let text = "";
+    let chunk = split_string(text.to_string(), ',', 1);
+    //let chunk = split_string("hello,world".to_string(), ',', 1);
     println!("Split string: {}", chunk);
 }
--- examples/13-function-arguments/values/src/main.rs
+++ examples/13-function-arguments/values/src/main.rs
@@ -7,9 +7,19 @@ fn sum(numbers: &[i32]) -> i32 {
     result
 }
 
+fn mean(numbers: &[i32]) -> i32 {
+    let mut result = 0;
+    for number in numbers {
+        result += number;
+    }
+    result / numbers.len() as i32
+}
+
 fn main() {
     // There are no variadic arguments in Rust
     let numbers = [1, 2, 3, 4, 5];
     let result = sum(&numbers);
     println!("The sum is {}", result);
+    let result = mean(&numbers);
+    println!("The mean is {}", result);
 }
